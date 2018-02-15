f7b4a99770636c8ab573d6e960ea718fd756c444
Joel Launder <jalaunder@gmail.com>
2018-02-15 21:24:49 +1100

    Added animations to stat counters

--- assets/js/statcounter.js
+++ assets/js/statcounter.js
@@ -7,14 +7,28 @@ function updateStats() {
     $.get(
         "https://haikubotapi.apphb.com/Api/haikucount",
         function (data) {
-            $(".haiku-counter").html(numberWithCommas(data));
+            haikus = numberWithCommas(data)
+            if ($(".haiku-counter").html() != haikus) {
+                $(".haiku-counter").addClass("hidden");
+                setTimeout(function () {
+                    $(".haiku-counter").html(haikus);
+                    $(".haiku-counter").removeClass("hidden");
+                }, 250);
+            }
         }
     );
 
     $.get(
         "https://discordbots.org/api/bots/372175794895585280/stats",
         function (data) {
-            $(".server-counter").html(numberWithCommas(data["server_count"]));
+            servers = numberWithCommas(data["server_count"])
+            if ($(".server-counter").html() != servers) {
+                $(".server-counter").addClass("hidden");
+                setTimeout(function () {
+                    $(".server-counter").html(servers);
+                    $(".server-counter").removeClass("hidden");
+                }, 250);
+            }
         }
     );
 }
