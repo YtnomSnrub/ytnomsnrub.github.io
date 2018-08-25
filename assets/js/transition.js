94c08ce290e06f9883ab542b146bb1230d2d89e9
Joel Launder <jalaunder@gmail.com>
2018-08-25 13:33:51 +1000

    Async javascript

--- assets/js/transition.js
+++ assets/js/transition.js
@@ -66,6 +66,18 @@ $(function () {
     addMarkdownAnchors();
 });
 
+function addMarkdownAnchors() {
+    return $("h2, h3, h4, h5, h6").each(function (i, el) {
+        var $el, icon, id;
+        $el = $(el);
+        id = $el.attr('id');
+        icon = '<i class="material-icons">link</i>';
+        if (id) {
+            return $el.append($("<a/>").addClass("header-link").attr("href", "#" + id).html(icon));
+        }
+    });
+}
+
 function updateTabs() {
     var $mainContent = $(".main-content");
     var $pageTabs = $("#PageTabs");
